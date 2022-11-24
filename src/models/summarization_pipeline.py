3bc01fd74011eae7fc240fbc0469a4995f1c62b4
wisoffe <wisoffe@gmail.com>
2022-11-24 16:38:22 +0300

    Preprocessing scripts development (complete summarization pipeline)

--- src/models/summarization_pipeline.py
+++ src/models/summarization_pipeline.py
@@ -4,13 +4,14 @@ import os
 import re
 import datetime
 from src.common_funcs import safe_pg_read_query, safe_pg_execute_values
+
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
 # Hyperparameters
 MODEL_CFG = {
     "name": "IlyaGusev/rut5_base_headline_gen_telegram",
-    "version": "v2022.10.19",
+    "id_model": 1,  # id_model in DB, task: change to get it by stage
     "text_prefix": "",
     "tokenizer_kwargs": {
         "max_length": 600,
@@ -24,11 +25,11 @@ MODEL_CFG = {
 PG_CONN_CFG = {
     "dbname": os.environ.get("POSTGRES_DB"),
     "user": os.environ.get("POSTGRES_USER"),
-    "port": 5432,
-    "host": "postgres",
+    "host": os.environ.get("POSTGRES_HOST"),
+    "port": os.environ.get("POSTGRES_PORT"),
 }
 with open(os.environ.get("POSTGRES_PASSWORD_FILE"), "r") as f:
-    PG_CONN_CFG["password"] = f.readlines()[0]
+    PG_CONN_CFG["password"] = f.readlines()[0].rstrip("\n")
 
 
 def inference(
@@ -58,53 +59,62 @@ def summarization_pipeline():
     news_to_summary = safe_pg_read_query(PG_CONN_CFG, query)
 
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # get summarization model
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_CFG["name"])
-    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_CFG["name"])
-
-    # processing one news at a time (do not use batch > 1 to save memory)
     result = []
-    for news_id, text, source in news_to_summary:
 
-        # clean text before input to model (custom by source or for all)
-        # if source = "src1":
-        #    text = re.sub(r"[^\x20-\xFFа-яА-ЯёЁ№\n]+|__|\*\*", '', text)
-        text = re.sub(r"[^\x20-\xFFа-яА-ЯёЁ№\n]+|__|\*\*", "", text)
+    # if news to processing exist
+    if len(news_to_summary) > 0:
 
-        # task: add if news is one simple sentence, then summary = clean text
+        # get summarization model
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_CFG["name"])
+        model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_CFG["name"])
 
-        summary = inference(
-            [text],
-            model,
-            tokenizer,
-            MODEL_CFG["tokenizer_kwargs"],
-            MODEL_CFG["generate_kwargs"],
-        )[0]
+        # processing one news at a time (do not use batch > 1 to save memory)
+        for news_id, text, source in news_to_summary:
 
-        # summary by model is incorrect if summary length > input text length
-        # in this case, use the original text
-        if len(summary) >= len(text):
-            summary = text
+            # clean text before input to model (custom by source or for all)
+            # if source = "src1":
+            #    text = re.sub(r"[^\x20-\xFFа-яА-ЯёЁ№\n]+|__|\*\*", '', text)
+            text = re.sub(r"[^\x20-\xFFа-яА-ЯёЁ№\n]+|__|\*\*", "", text)
 
-        result.append(
-            (
-                news_id,
-                current_date,
-                " ".join([MODEL_CFG["name"], MODEL_CFG["version"]]),
-                summary,
+            # task: add if news is one simple sentence, then summary = clean text
+
+            summary = inference(
+                [text],
+                model,
+                tokenizer,
+                MODEL_CFG["tokenizer_kwargs"],
+                MODEL_CFG["generate_kwargs"],
+            )[0]
+
+            # summary by model is incorrect if summary length > input text length
+            # in this case, use the original text
+            if len(summary) >= len(text):
+                summary = text
+
+            result.append(
+                (
+                    news_id,
+                    current_date,
+                    summary,
+                    MODEL_CFG["id_model"],
+                )
             )
+
+        del model, tokenizer
+
+        # save summarization results in database
+        query = """
+            INSERT INTO news_summary(id_news, date_generated,
+                                    summary_text, id_model)
+            VALUES %s
+        """
+        safe_pg_execute_values(PG_CONN_CFG, query, result)
+
+    print(
+        "Summarization pipeline completed: {} - processed {} news".format(
+            current_date, len(result)
         )
-
-    del model, tokenizer
-
-    # save summarization results in database
-    query = """
-        INSERT INTO news_summary(id_news, date_generated,
-                                 model_version, summary_text)
-        VALUES %s
-    """
-    safe_pg_execute_values(PG_CONN_CFG, query, result)
+    )
 
 
 if __name__ == "__main__":
