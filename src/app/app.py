1182d1b9aa39125bfa877c8e72874cc3c7a21243
wisoffe <wisoffe@gmail.com>
2022-11-30 17:06:46 +0300

    Complete pre-release frontend app

--- src/app/app.py
+++ src/app/app.py
@@ -1,48 +1,245 @@
+import uuid
+import re
+import os
+from itertools import combinations
 from flask import Flask, render_template, request, jsonify, session
 import pandas as pd
 import networkx as nx
-import uuid
+import psycopg2
+from psycopg2 import Error
 
+
+# Hyperparameters
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['SECRET_KEY'] = uuid.uuid4().hex
-app.config['SECRET_KEY'] = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
+app.config["SECRET_KEY"] = uuid.uuid4().hex
+
+PG_CONN_CFG = {
+    "dbname": os.environ.get("POSTGRES_DB"),
+    "user": os.environ.get("POSTGRES_USER"),
+    "host": os.environ.get("POSTGRES_HOST"),
+    "port": os.environ.get("POSTGRES_PORT"),
+}
+with open(os.environ.get("POSTGRES_PASSWORD_FILE"), "r") as f:
+    PG_CONN_CFG["password"] = f.readlines()[0].rstrip("\n")
+
+
+def get_db_data_for_triplets(pg_conn_cfg, date_min: str, date_max: str):
+
+    # check query dates to sql injection
+    date_pattern = r"\d\d\d\d-\d\d-\d\d"
+    if re.fullmatch(date_pattern, date_min) and re.fullmatch(date_pattern, date_max):
+        try:
+            pg_con = psycopg2.connect(
+                dbname=pg_conn_cfg["dbname"],
+                user=pg_conn_cfg["user"],
+                password=pg_conn_cfg["password"],
+                host=pg_conn_cfg["host"],
+                port=pg_conn_cfg["port"],
+            )
+            query = f"""
+                SELECT id_news, summary_text, news_date
+                FROM (SELECT * FROM news
+                    WHERE news_date
+                            BETWEEN '{date_min}' AND
+                                    '{date_max}') news
+                    INNER JOIN news_summary USING(id_news)
+            """
+            df_news = pd.read_sql(query, pg_con, index_col=["id_news"])
+
+            query = f"""
+                SELECT id_news, ner_name
+                FROM (SELECT * FROM news_links
+                    WHERE id_news IN (SELECT id_news FROM news
+                                        WHERE news_date
+                                        BETWEEN '{date_min}' AND
+                                        '{date_max}')) news_links
+                    INNER JOIN ner USING(id_ner)
+            """
+            df_nlinks = pd.read_sql(query, pg_con)
+
+        except (Exception, Error) as error:
+            print("Error connection to PostgreSQL:\n", error)
+            df_news = None
+            df_nlinks = None
+
+        finally:
+            if "pg_con" in locals() and pg_con:
+                pg_con.close()
+    else:
+        df_news = None
+        df_nlinks = None
+        print(f"Dates is incorrect {date_min} - {date_max}")
+
+    return df_news, df_nlinks
+
+
+def compute_triplets(
+    PG_CONN_CFG,
+    input_ner,
+    date_min: str,
+    date_max: str,
+    graph_depth=None,
+    min_news_count=1,
+):
+    # query to db
+    df_news, df_nlinks = get_db_data_for_triplets(PG_CONN_CFG, date_min, date_max)
+
+    # task: change to fuzzy search by synonyms table
+    founded_ner = input_ner
+
+    if (
+        df_news is None
+        or df_nlinks is None
+        or (input_ner != "" and founded_ner not in df_nlinks.ner_name.values)
+    ):
+        return pd.DataFrame(
+            {
+                "source": ["no_node1"],
+                "target": ["no_node2"],
+                "edge": ["0 news"],
+                "amount": [0],
+                "news": [["no news"]],
+            }
+        )
+
+    df_nlinks_counts = df_nlinks.groupby(by="id_news").ner_name.count()
+
+    # удаляем новости с >= 10 нер, пока не решится вопрос с комплексными
+    # сводками новостей (часто с ключевым словом "главное:"), где могут
+    # в виде списка приводится не связанные между собой новости,
+    # в настоящий момент все ner, упоминаемые в любой части тако сводки,
+    # окажутся связанными между собой, что не является верным; имеет смысл
+    # дробить такие новости на несколько, либо использовать схожий подход,
+    # когда ner будут связаны между обой только в пределах перечислений
+    id_news_to_drop = df_nlinks_counts[
+        ((df_nlinks_counts < 2) | (df_nlinks_counts > 10))
+    ].index
+    df_nlinks = df_nlinks[~df_nlinks.id_news.isin(id_news_to_drop)]
+
+    if input_ner != "":
+
+        lvl_ners = [
+            [founded_ner],
+        ]  # functionality for future
+        lvl_idx = []  # functionality for future
+        prev_lvls_idx = []
+
+        for _ in range(graph_depth):
+
+            lvl_idx.append(
+                list(
+                    set(
+                        df_nlinks[
+                            (
+                                df_nlinks.ner_name.isin(lvl_ners[-1])
+                                & (~df_nlinks.id_news.isin(prev_lvls_idx))
+                            )
+                        ].id_news
+                    )
+                )
+            )
+
+            lvl_ners.append(
+                list(
+                    set(df_nlinks[df_nlinks.id_news.isin(lvl_idx[-1])].ner_name)
+                    - set(lvl_ners[-1])
+                )
+            )
+
+            prev_lvls_idx.extend(lvl_idx[-1])
+
+        # print(list(map(len, lvl_idx)))
+        # print(list(map(len, lvl_ners)))
+
+        df_nlinks = df_nlinks[df_nlinks.id_news.isin(prev_lvls_idx)]
+
+    df_nlinks = df_nlinks.groupby("id_news").agg({"ner_name": sorted})
+
+    df_nlinks = df_nlinks.merge(df_news, how="left", left_index=True, right_index=True)
+    del df_news
+
+    df_nlinks["ner_name"] = df_nlinks.ner_name.map(lambda x: list(combinations(x, 2)))
+
+    df_nlinks["news"] = df_nlinks.news_date.astype(str) + ": " + df_nlinks.summary_text
+    df_nlinks = df_nlinks[["ner_name", "news"]]
+
+    df_triples = df_nlinks.explode("ner_name")
+    del df_nlinks
+
+    df_triples = df_triples.groupby("ner_name", as_index=False).agg(
+        news=("news", sorted), amount=("news", len)
+    )
+
+    # drop edges with news amount < min_news_count
+    if min_news_count > 1:
+        df_triples = df_triples[df_triples.amount >= min_news_count]
+
+    df_triples["edge"] = df_triples.amount.map(lambda x: f"{x} news")
+
+    df_triples[["source", "target"]] = pd.DataFrame(
+        df_triples["ner_name"].tolist(), index=df_triples.index
+    )
+
+    df_triples = df_triples[["source", "target", "edge", "amount", "news"]]
+
+    return df_triples
 
 
 def build_network(graph_query):
-    print("BUILD_NETWORK_FUNK:", graph_query)
-    df = pd.read_csv('example_graph.csv')
-    G = nx.from_pandas_edgelist(df, source='source', target='target', edge_attr='name', create_using=nx.Graph())
+
+    df_triples = compute_triplets(PG_CONN_CFG, **graph_query)
+    df_triples = df_triples.rename(columns={"edge": "name"})[
+        ["source", "target", "name"]
+    ]
+
+    G = nx.from_pandas_edgelist(
+        df_triples,
+        source="source",
+        target="target",
+        edge_attr="name",
+        create_using=nx.Graph(),
+    )
     data = nx.node_link_data(G)
 
     return data
 
 
 # DELETE BEFORE PROD!!!
-@app.route("/temp", methods=['GET', 'POST'])
+@app.route("/temp", methods=["GET", "POST"])
 def chrt():
-    return render_template('temp2.html')
+    return render_template("temp2.html")
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index_func():
     if request.method == "POST":
         graph_query = {
-            "input_ner": request.form.get('input_ner'),
-            "date_min": request.form.get('date_min'),
-            "date_max": request.form.get('date_max'),
-            "graph_depth": request.form.get('graph_depth'),
-            "min_news_count": request.form.get('min_news_count'),
+            "input_ner": request.form.get("input_ner"),
+            "date_min": request.form.get("date_min"),
+            "date_max": request.form.get("date_max"),
+            "graph_depth": int(request.form.get("graph_depth")),
+            "min_news_count": int(request.form.get("min_news_count")),
         }
-        session['graph_query'] = graph_query
+    else:
+        graph_query = {
+            "input_ner": "",
+            "date_min": "2022-08-01",
+            "date_max": "2022-08-31",
+            "min_news_count": 10,
+        }
 
-    return render_template('index.html')
+    session["graph_query"] = graph_query
+
+    print(f"Info: Graph query: {graph_query}")
+
+    return render_template("index.html")
 
 
 @app.route("/data")
 def static_proxy():
-    network = build_network(session.get('graph_query'))
+    network = build_network(session.get("graph_query"))
 
     return jsonify(network)
 
@@ -51,5 +248,5 @@ def main():
     app.run(host="0.0.0.0", debug=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/app/static/js/index.js
+++ src/app/static/js/index.js
@@ -1,9 +1,9 @@
 var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
 
 svg.append("svg:defs").selectAll("marker")
-    .data(["end"])      // Different link/path types can be defined here
+  .data(["end"])      // Different link/path types can be defined here
   /*.enter().append("svg:marker")    // This section adds in the arrows : Стрелки (часть 1 из 2)
     .attr("id", String)
     .attr("viewBox", "0 -5 10 10")
@@ -14,7 +14,7 @@ svg.append("svg:defs").selectAll("marker")
     .attr("orient", "auto")
     .attr('fill', '#A0A0A0') // #A0A0A0 серый, #00000 черный */
   .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5")
+  .attr("d", "M0,-5L10,0L0,5")
 
 
 
@@ -22,14 +22,14 @@ svg.append("svg:defs").selectAll("marker")
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
-  .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  .force("charge", d3.forceManyBody().strength(-2000)) // мера зазреженности графа (-100 очень ужат, -2000 разрежен)
+  .force("link", d3.forceLink().id(function (d) { return d.id; }))
+  .force("charge", d3.forceManyBody().strength(-800)) // мера зазреженности графа (-100 очень ужат, -2000 разрежен)
   .force("center", d3.forceCenter(width / 2, height / 2));
 
 
 d3.json("/data", function (error, graph) {
 
-    if (error) throw error;
+  if (error) throw error;
 
   var line = svg.append("g")
     .attr("class", "links")
@@ -43,7 +43,7 @@ d3.json("/data", function (error, graph) {
     .attr('fill', 'black')
     .attr('stroke', '#A0A0A0')
     .attr("stroke-width", 1)
-    //.attr('stroke-opacity', 1).attr("marker-end", "url(#end)") // Стрелки (часть 2 из 2)
+  //.attr('stroke-opacity', 1).attr("marker-end", "url(#end)") // Стрелки (часть 2 из 2)
 
 
   var node = svg.append("g")
@@ -51,19 +51,19 @@ d3.json("/data", function (error, graph) {
     .selectAll("g")
     .data(graph.nodes)
     .enter().append("g");
-    // изменение прозрачности вершин
+  // изменение прозрачности вершин
 
   var circle = node.append('circle')
     .attr('r', 10)
     .attr('stroke', 'black')
-    .attr('fill', function(d,i){return color(i);})
+    .attr('fill', function (d, i) { return color(i); })
 
   var labels = node.append("text")
-      .text(function(d) {
-        return d.id;
-      })
-      .attr('x', 12)
-      .attr('y', 3);
+    .text(function (d) {
+      return d.id;
+    })
+    .attr('x', 12)
+    .attr('y', 3);
   // Create a drag handler and append it to the node object instead
   var drag_handler = d3.drag()
     .on("start", dragstarted)
@@ -71,54 +71,54 @@ d3.json("/data", function (error, graph) {
     .on("end", dragended);
 
   drag_handler(node);
-  
+
   var linelabels = line
-      .append('text')
-        .attr('text-anchor', 'middle')
-        .attr("x", 30)
-        .attr("y", 30)
-        .text(function(d) {
-          return d.name;
-        })
-        .attr('opacity', 0.2)
-        .on('mouseover', function (d, i) {
-          d3.select(this).transition()
-               .duration('50')
-               .attr('opacity', '.0.99');
-        
-          })
-          .on('mouseout', function (d, i) {
-                d3.select(this).transition()
-                    .duration('50')
-                    .attr('opacity', '0.2');
-    
-          });
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr("x", 30)
+    .attr("y", 30)
+    .text(function (d) {
+      return d.name;
+    })
+    .attr('opacity', 0.2)
+    .on('mouseover', function (d, i) {
+      d3.select(this).transition()
+        .duration('50')
+        .attr('opacity', '.0.99');
+
+    })
+    .on('mouseout', function (d, i) {
+      d3.select(this).transition()
+        .duration('50')
+        .attr('opacity', '0.2');
+
+    });
 
 
 
 
   simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+    .nodes(graph.nodes)
+    .on("tick", ticked);
 
   simulation.force("link")
-      .links(graph.links);
-    
+    .links(graph.links);
+
 
   function ticked() {
     link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+      .attr("x1", function (d) { return d.source.x; })
+      .attr("y1", function (d) { return d.source.y; })
+      .attr("x2", function (d) { return d.target.x; })
+      .attr("y2", function (d) { return d.target.y; });
 
     node
-        .attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        })
+      .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
     linelabels
-        .attr("x", function(d) { return (d.source.x+d.target.x)/2; })
-        .attr("y", function(d) { return (d.source.y+d.target.y)/2; })
+      .attr("x", function (d) { return (d.source.x + d.target.x) / 2; })
+      .attr("y", function (d) { return (d.source.y + d.target.y) / 2; })
 
   }
 });
