3a5a6b00e84131461cdde27032174d4b34237544
wisoffe <wisoffe@gmail.com>
2022-12-12 09:29:56 +0300

    Update news_db schema (add ner types and synonym name_for_match)

--- src/data/create_db_schema.sql
+++ src/data/create_db_schema.sql
@@ -12,12 +12,20 @@ CREATE TABLE news (
     date_collected timestamp NOT NULL
     );
 
+--Create ner_types table
+CREATE TABLE ner_types (
+    id_ner_type SERIAL NOT NULL PRIMARY KEY,
+    ner_type VARCHAR(30) NOT NULL
+    );
+
 --Create ner table
 CREATE TABLE ner (
     id_ner SERIAL NOT NULL PRIMARY KEY,
     ner_name TEXT NOT NULL,
     qid_wikidata TEXT,
-    name_is_custom INTEGER
+    name_is_custom INTEGER,
+    id_ner_type INTEGER,
+    FOREIGN KEY (id_ner_type) REFERENCES ner_types (id_ner_type);
     );
 
 --Create news_links table
@@ -34,6 +42,7 @@ CREATE TABLE ner_synonyms (
     id_synonim SERIAL NOT NULL PRIMARY KEY,
     id_ner INTEGER NOT NULL,
     ner_synonym TEXT NOT NULL,
+    name_for_match TEXT,
     FOREIGN KEY (id_ner) REFERENCES ner (id_ner) ON DELETE CASCADE
     );
 
@@ -80,8 +89,10 @@ CREATE TABLE synonyms_stats (
     news_count INTEGER NOT NULL,
     date_processed timestamp NOT NULL,
     id_model INTEGER NOT NULL,
+    id_ner_type INTEGER,
     FOREIGN KEY (id_synonim) REFERENCES ner_synonyms (id_synonim) ON DELETE CASCADE,
-    FOREIGN KEY (id_model) REFERENCES models (id_model)
+    FOREIGN KEY (id_model) REFERENCES models (id_model),
+    FOREIGN KEY (id_ner_type) REFERENCES ner_types (id_ner_type)
     );
 
 --Add default model stages
@@ -93,6 +104,13 @@ VALUES ('production'),
 INSERT INTO model_types(model_type)
 VALUES ('summarization'),
         ('ner');
+
+--Add default ner types
+INSERT INTO ner_types(id_ner_type, ner_type)
+VALUES (1, 'PER'),
+        (2, 'LOC'),
+        (3, 'ORG'),
+        (4, 'MISC');
 
 --for fuzzy search by SIMILARITY
 CREATE EXTENSION pg_trgm;
